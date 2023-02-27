e479f75e976484bc9f9aaf67da2510d26fdc76f6
Rob Vanarsdall <rvanar@gmail.com>
2023-02-27 11:15:55 -0500

    merge with main

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-    <div class="container-fluid text-center text-white p-5 bg-primary">
+    <div className="container-fluid text-center text-white p-5 bg-primary">
       <h1>Next JS Examples</h1>
     </div>
   );
